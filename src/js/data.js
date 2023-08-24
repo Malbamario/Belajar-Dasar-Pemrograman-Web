4460543c8b49bcd3c3c83c42fbbd60f903bd052f
Malbamario <malba324@gmail.com>
2023-08-25 00:49:32 +0800

    Add profile pic, resin and font, and make footer

--- src/js/data.js
+++ src/js/data.js
@@ -1,38 +1,38 @@
 data = {
     user_data: {
-        name : "Malbamario",
-        age : "20"
+        name: "Malbamario",
+        resin: 25,
+        img_name: "Aether_Icon.webp",
     },
     article_data: [
         {
             img: "maxresdefault.jpg",
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
             title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba"
+            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
-    ]
+    ],
 };
-
--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
+// Get the navbar
+var navbar = document.getElementById("navbar");
+document.getElementById("mainEl").style.marginTop = navbar.offsetHeight.toString() + "px";
+document.getElementById("img_profil").style.backgroundImage =
+    "url(./src/img/" + data.user_data.img_name + ")";
+
 const usernameEl = document.getElementById("username_card");
+const resinEl = document.getElementById("resinBar");
+const resinLabel = document.getElementById("resinLabel");
 const articlesEl = document.getElementById("articles");
 
 usernameEl.innerText = data.user_data.name;
+resinEl.value = data.user_data.resin;
+resinLabel.innerText = data.user_data.resin + "/160";
 
-
-
-let articlesInner = '';
-data.article_data.forEach(e => {
-    articlesInner += `<article><div class="thumbnail"><img src="./src/img/${e.img}"></div><div class="text"><h2>${e.title}</h2><p>${e.tease}</p></div></article>`;
+let articlesInner = "";
+data.article_data.forEach((e) => {
+    articlesInner += `<article><img class="thumbnail" src="./src/img/${e.img}"><div class="text"><h2>${e.title}</h2><p>${e.tease}</p></div></article>`;
 });
 
 articlesEl.innerHTML = articlesInner;
-
-// if (data.dataset.name==="main-data"){
-//     console.log("main-data loaded")
-
-// }
-
-console.log(data);
