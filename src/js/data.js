06840159fbe4823f41ab03be77d44b62f42c5e36
Malbamario <malba324@gmail.com>
2023-08-25 11:11:44 +0800

    Add articles with the source, and source of assets

--- src/js/data.js
+++ src/js/data.js
@@ -11,28 +11,29 @@ data = {
             tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
         },
         {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
+            img: "maxresdefault (1).jpg",
+            title: "Guide for Collect All 85 Hydroculus Locations",
+            tease: "Hello I'm takaGG. This is a guidance of the route to collect all the Hydroculus in Fontaine for version 4.0.",
         },
         {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
+            img: "melt-team-1024x573.jpg",
+            title: "My Melt Team Build",
+            tease: "Do you have a strugle in build a melting team? I have some combination of character for this team build type ",
         },
         {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
+            img: "gnlq3mwe9lk81.webp",
+            title: "Comparing Jean vs Kazuha",
+            tease: "Wich one is better, Jean or Kazuha? Well, Hello travelers welcome back with me Lesterberne. This 2 sword-wielding Anemo characters have a such of different mechanics.",
         },
         {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
+            img: "202010218733.0.webp",
+            title: "Genshin Impact beginner’s guide, tips, and tricks",
+            tease: "Whether you button-mashed through the tutorials or not, these tips will be useful.",
         },
         {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
-        },
-        {
-            title: "Event Version 4.0",
-            tease: "Halo para travelers! Momen yang ditunggu-tunggu telah tiba",
+            img: "d3abaaae-a677-4059-9380-5c77fbfc0fc5-hu-tao-pray.avif",
+            title: "5 tips to optimize your artifacts in Genshin Impact",
+            tease: "We know, you’re trying your best. Read the character descriptions · HP and DEF do matter · Avoid investing in flat stats · BUILD WITH PURPOSE, EVEN IF IT’S NOT “META” · KNOW WHEN TO SETTLE · ",
         },
     ],
 };
